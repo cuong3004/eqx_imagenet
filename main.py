df79d9d8d77da9afe5653063a35abe32c6753af5
cuong3004 <duongduccuong30042002@gmail.com>
2024-04-08 03:27:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ class LitResnet(LightningModule):
         schedule = optax.warmup_cosine_decay_schedule(
             init_value=0.0,
             peak_value=0.1,
-            warmup_steps=3*args["train_step_epoch"],
+            warmup_steps=4*args["train_step_epoch"],
             decay_steps=80*args["train_step_epoch"],
             end_value=0.0001,
         )
